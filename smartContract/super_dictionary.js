2f5811523d6c966e592fa6471f2ccef0424e5ca7
GongJS <382663074@qq.com>
2018-05-12 03:52:09 +0800

    初版设计

--- smartContract/super_dictionary.js
+++ smartContract/super_dictionary.js
@@ -1,71 +1,43 @@
-"use strict";
-
-var DictItem = function(text) {
-	if (text) {
-		var obj = JSON.parse(text);
-		this.key = obj.key;
-		this.value = obj.value;
-		this.author = obj.author;
-	} else {
-	    this.key = "";
-	    this.author = "";
-	    this.value = "";
-	}
-};
-
-DictItem.prototype = {
-	toString: function () {
-		return JSON.stringify(this);
-	}
-};
-
-var SuperDictionary = function () {
-    LocalContractStorage.defineMapProperty(this, "repo", {
-        parse: function (text) {
-            return new DictItem(text);
-        },
-        stringify: function (o) {
-            return o.toString();
-        }
-    });
-};
-
-SuperDictionary.prototype = {
-    init: function () {
-        // todo
-    },
-
-    save: function (key, value) {
-
-        key = key.trim();
-        value = value.trim();
-        if (key === "" || value === ""){
-            throw new Error("empty key / value");
-        }
-        if (value.length > 64 || key.length > 64){
-            throw new Error("key / value exceed limit length")
-        }
-
+'use strict';
+var SampleContract = function() {};
+SampleContract.prototype = {
+    init: function() {},
+    clipIn: function(number) {
+        number = parseInt(number);
         var from = Blockchain.transaction.from;
-        var dictItem = this.repo.get(key);
-        if (dictItem){
-            throw new Error("value has been occupied");
+        var value = Blockchain.transaction.value;
+        var resultNumber = parseInt(Math.floor(Math.random() * 10 + 1));
+        if (value !== 1000000000000000000) { throw new Error("必须支付1个NAS");}
+        if (number == resultNumber) {
+            var result = Blockchain.transfer(from, 10);
+            if(result == true) {
+                console.log("你猜对了，获得10个NAS奖励");
+                return true;
+            }else{
+                 throw new Error("奖励领取失败，请联系管理员");
+            }
+        } else {
+            console.log("你猜错了，正确答案是" + resultNumber);
+            return resultNumber;
         }
-
-        dictItem = new DictItem();
-        dictItem.author = from;
-        dictItem.key = key;
-        dictItem.value = value;
-
-        this.repo.put(key, dictItem);
     },
-
-    get: function (key) {
-        key = key.trim();
-        if ( key === "" ) {
-            throw new Error("empty key")
+    withDraw: function(amount) {
+        amount = parseInt(amount);
+        var user = Blockchain.transaction.from;
+        if (user == "n1LihoAbPc6xvxZq1pHupw3ASnWMaJgm6cG") {
+            var withDrawResult = Blockchain.transfer(user, amount);
+            if (withDrawResult == true) {
+                console.log("提款成功");
+                return true;
+            } else {
+                console.log("提款失败");
+                throw new Error("提款失败");
+            }
+        } else {
+            console.log("你没有权限");
+            throw new Error("你没有权限");
         }
-        return this.repo.get(key);
     }
 };
-module.exports = SuperDictionary;
+
+module.exports = SampleContract;
