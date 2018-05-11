2ab3274a5809029922267f80a12fa0df302c7827
GongJS <382663074@qq.com>
2018-05-12 04:21:49 +0800

    添加注释

--- smartContract/super_dictionary.js
+++ smartContract/super_dictionary.js
@@ -2,13 +2,19 @@
 var SampleContract = function() {};
 SampleContract.prototype = {
     init: function() {},
+    //获取前端传入的数字，并与内部随机生成的数字进行比较
     clipIn: function(number) {
         number = parseInt(number);
+        //获取使用者的地址
         var from = Blockchain.transaction.from;
+        //获取使用者支付的nas数值
         var value = Blockchain.transaction.value;
+        //随机生成1-10的数字
         var resultNumber = parseInt(Math.floor(Math.random() * 10 + 1));
+        //判断使用者是否支付1个NAS
         if (value !== 1000000000000000000) { throw new Error("必须支付1个NAS");}
         if (number == resultNumber) {
+            //给使用者奖励10个NAS
             var result = Blockchain.transfer(from, 10);
             if(result == true) {
                 console.log("你猜对了，获得10个NAS奖励");
@@ -24,6 +30,7 @@ SampleContract.prototype = {
     withDraw: function(amount) {
         amount = parseInt(amount);
         var user = Blockchain.transaction.from;
+        //判断是否是指定的账户地址
         if (user == "n1LihoAbPc6xvxZq1pHupw3ASnWMaJgm6cG") {
             var withDrawResult = Blockchain.transfer(user, amount);
             if (withDrawResult == true) {
